158650b5870d6bd2db7688b78b6b1d468ae253be
douglas_barraza <douglas.barraza@externos-cl.cencosud.com>
2022-06-11 21:20:53 -0400

    Created contianer deploy script

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const configProd = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Marketing': './src/bootstrap',
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, configProd);
